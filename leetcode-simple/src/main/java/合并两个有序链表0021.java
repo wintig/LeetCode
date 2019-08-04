c48503c4a065b7fb02f42cd1101e7f2ff3558b68
wintig <840872473@qq.com>
2019-08-05 01:47:08 +0800

    合并两个有序链表0021

--- leetcode-simple/src/main/java/合并两个有序链表0021.java
+++ leetcode-simple/src/main/java/合并两个有序链表0021.java
@@ -2,20 +2,49 @@
 
 public class 合并两个有序链表0021 {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public static void main(String[] args) {
 
-        
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
 
-        return null;
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
 
+        ListNode listNode = mergeTwoLists(l1, l2);
+
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
     }
 
 }
 
-
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
