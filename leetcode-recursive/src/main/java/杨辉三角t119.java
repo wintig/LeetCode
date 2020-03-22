c562826ce5249d532261316beb5c05c9c7dc2af9
wintig <840872473@qq.com>
2020-03-22 22:54:24 +0800

    杨辉三角2

--- leetcode-recursive/src/main/java/杨辉三角t119.java
+++ leetcode-recursive/src/main/java/杨辉三角t119.java
@@ -20,6 +20,8 @@ public class 杨辉三角t119 {
 
         row.add(1);
         for (int i = 1; i <= rowIndex; i++) {
+
+            // 这里需要从后往前，如果从前往后的话，数据会被覆盖
             for (int j = i - 1; j > 0; j--) {
                 row.set(j, row.get(j - 1) + row.get(j));
             }
